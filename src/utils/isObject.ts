a011d4e28ece3efe83690f80e4ac04d97a59b849
Petr Tripolsky <tripolskypetr@gmail.com>
2021-01-29 17:28:11 +0300

    isObject upgrade

--- src/utils/isObject.ts
+++ src/utils/isObject.ts
@@ -1,6 +1,9 @@
-export const isObject = (obj: any) => {
-    const type = typeof obj;
-    return type === 'function' || type === 'object' && !!obj;
+export const isObject = (obj: any): boolean => {
+    if (typeof obj === 'object' && obj !== null) {
+        return Object.getPrototypeOf(obj) === Object.prototype;
+    } else {
+        return false;
+    }
 };
 
 export default isObject;
