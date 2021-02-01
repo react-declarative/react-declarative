f074d69f7ab672973bda2ee8d465af2007e17e80
Petr Tripolsky <tripolskypetr@gmail.com>
2021-02-01 15:06:11 +0300

    deepFlat-tests

---
+++ src/utils/__tests__/deepFlat.test.ts
@@ -0,0 +1,393 @@
+import deepFlat from "../deepFlat";
+
+import TypedField from "../../model/TypedField";
+import FieldType from "../../model/FieldType";
+
+describe("Create object flatmap by field", () => {
+  const fields32: TypedField[] = [
+    {
+      type: FieldType.Group,
+      fields: [
+        {
+          type: FieldType.Group,
+          fields: [
+            {
+              type: FieldType.Group,
+              fields: [
+                {
+                  type: FieldType.Group,
+                },
+                {
+                  type: FieldType.Group,
+                },
+              ],
+            },
+            {
+              type: FieldType.Group,
+              fields: [
+                {
+                  type: FieldType.Group,
+                },
+                {
+                  type: FieldType.Group,
+                },
+                {
+                  type: FieldType.Group,
+                },
+                {
+                  type: FieldType.Group,
+                  fields: [
+                    {
+                      type: FieldType.Group,
+                      fields: [
+                        {
+                          type: FieldType.Group,
+                        },
+                      ],
+                    },
+                    {
+                      type: FieldType.Group,
+                      fields: [
+                        {
+                          type: FieldType.Group,
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: FieldType.Group,
+        },
+        {
+          type: FieldType.Group,
+        },
+        {
+          type: FieldType.Group,
+        },
+        {
+          type: FieldType.Group,
+        },
+        {
+          type: FieldType.Group,
+          fields: [
+            {
+              type: FieldType.Group,
+            },
+            {
+              type: FieldType.Group,
+            },
+          ],
+        },
+        {
+          type: FieldType.Group,
+          fields: [
+            {
+              type: FieldType.Group,
+              fields: [
+                {
+                  type: FieldType.Group,
+                },
+                {
+                  type: FieldType.Group,
+                },
+                {
+                  type: FieldType.Group,
+                },
+              ],
+            },
+            {
+              type: FieldType.Group,
+              fields: [
+                {
+                  type: FieldType.Group,
+                },
+                {
+                  type: FieldType.Group,
+                },
+                {
+                  type: FieldType.Group,
+                },
+                {
+                  type: FieldType.Group,
+                },
+                {
+                  type: FieldType.Group,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ];
+  const fields3: TypedField[] = [
+    {
+      type: FieldType.Group,
+    },
+    {
+      type: FieldType.Group,
+    },
+    {
+      type: FieldType.Group,
+    },
+  ];
+  const fields50: TypedField[] = [
+    {
+        type: FieldType.Group,
+        fields: [
+          {
+            type: FieldType.Group,
+          },
+          {
+            type: FieldType.Group,
+          },
+          {
+            type: FieldType.Group,
+          },
+          {
+            type: FieldType.Group,
+          },
+          {
+            type: FieldType.Group,
+          },
+          {
+            type: FieldType.Group,
+          },
+          {
+            type: FieldType.Group,
+            fields: [
+              {
+                type: FieldType.Group,
+              },
+              {
+                type: FieldType.Group,
+              },
+              {
+                type: FieldType.Group,
+              },
+              {
+                type: FieldType.Group,
+              },
+              {
+                type: FieldType.Group,
+              },
+              {
+                type: FieldType.Group,
+              },
+              {
+                type: FieldType.Group,
+              },
+              {
+                type: FieldType.Group,
+              },
+              {
+                type: FieldType.Group,
+                fields: [
+                  {
+                    type: FieldType.Group,
+                  },
+                  {
+                    type: FieldType.Group,
+                    fields: [
+                      {
+                        type: FieldType.Group,
+                      },
+                      {
+                        type: FieldType.Group,
+                      },
+                      {
+                        type: FieldType.Group,
+                        fields: [
+                          {
+                            type: FieldType.Group,
+                          },
+                          {
+                            type: FieldType.Group,
+                          },
+                          {
+                            type: FieldType.Group,
+                          }
+                        ]
+                      }
+                    ]
+                  },
+                  {
+                    type: FieldType.Group,
+                    columns: "6",
+                    fields: [
+                      {
+                        type: FieldType.Group,
+                      },
+                      {
+                        type: FieldType.Group,
+                      },
+                      {
+                        type: FieldType.Group,
+                      },
+                      {
+                        type: FieldType.Group,
+                      },
+                      {
+                        type: FieldType.Group,
+                      },
+                      {
+                        type: FieldType.Group,
+                      },
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        type: FieldType.Group,
+        fields: [
+          {
+            type: FieldType.Group,
+          },
+          {
+            type: FieldType.Group,
+          },
+        ]
+      },
+      {
+        type: FieldType.Group,
+        fields: [
+          {
+            type: FieldType.Group,
+          },
+          {
+            type: FieldType.Group,
+          },
+          {
+            type: FieldType.Group,
+          },
+        ]
+      },
+      {
+        type: FieldType.Group,
+      },
+      {
+        type: FieldType.Group,
+      },
+      {
+        type: FieldType.Group,
+      },
+      {
+        type: FieldType.Group,
+        fields: [
+          {
+            type: FieldType.Group,
+            fields: [
+              {
+                type: FieldType.Group,
+              },
+            ]
+          },
+          {
+            type: FieldType.Group,
+            columns: "6",
+            fields: [
+              {
+                type: FieldType.Group,
+              },
+            ]
+          },
+        ]
+      },
+      {
+        type: FieldType.Group,
+      },
+      {
+        type: FieldType.Group,
+      },
+      {
+        type: FieldType.Group,
+      }
+  ];
+  const fields19: TypedField[] = [
+    {
+      type: FieldType.Group,
+      fields: [
+        {
+          type: FieldType.Group,
+          fields: [
+            {
+              type: FieldType.Group,
+            },
+            {
+              type: FieldType.Group,
+            },
+            {
+              type: FieldType.Group,
+              fields: [
+                {
+                  type: FieldType.Group,
+                },
+              ],
+            },
+            {
+              type: FieldType.Group,
+            },
+          ],
+        },
+        {
+          type: FieldType.Group,
+          fields: [
+            {
+              type: FieldType.Group,
+            },
+            {
+              type: FieldType.Group,
+            }
+          ],
+        },
+      ],
+    },
+    {
+      type: FieldType.Group,
+      fields: [
+        {
+          type: FieldType.Group,
+          fields: [
+            {
+              type: FieldType.Group,
+            },
+            {
+              type: FieldType.Group,
+            },
+          ],
+        },
+        {
+          type: FieldType.Group,
+          fields: [
+            {
+              type: FieldType.Group,
+            },
+            {
+              type: FieldType.Group,
+            },
+            {
+              type: FieldType.Group,
+            },
+            {
+              type: FieldType.Group,
+            },
+          ],
+        },
+      ],
+    },
+  ];
+  it ('Will process deepFlat testcases', () => {
+    expect(deepFlat(fields50, 'fields').length).toBe(50);
+    expect(deepFlat(fields32, 'fields').length).toBe(32);
+    expect(deepFlat(fields19, 'fields').length).toBe(19);
+    expect(deepFlat(fields3, 'fields').length).toBe(3);
+  });
+});
