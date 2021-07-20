681cbc74bf1738858f418ea6480c186405b1bad6
Petr Tripolsky <tripolskypetr@gmail.com>
2021-07-20 18:33:25 +0300

    example-fix

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import { i18nMap } from 'react-view-builder';
+import { i18nMap, ModalProvider } from 'react-view-builder';
 
 i18nMap["OK"] = "Применить";
 i18nMap["Cancel"] = "Отмена";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <ModalProvider>
+        <App />
+    </ModalProvider>,
+    document.getElementById('root')
+);
