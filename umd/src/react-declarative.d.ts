b958c6ee438bbb9727d4de2ea537800b9d6e8915
Petr Tripolsky <tripolskypetr@gmail.com>
2021-09-07 02:53:03 +0300

    decl

--- umd/src/react-declarative.d.ts
+++ umd/src/react-declarative.d.ts
@@ -724,8 +724,9 @@ declare module 'react-declarative/model/IRowData' {
 declare module 'react-declarative/components/Switch/model/ISwitchItem' {
     export interface ISwitchItem {
         path: string;
-        component: React.ComponentType<any>;
+        component?: React.ComponentType<any>;
         guard?: () => boolean;
+        redirect?: string;
     }
     export default ISwitchItem;
 }
@@ -1700,7 +1701,7 @@ declare module 'react-declarative/components/List/components/PropProvider' {
 
 declare module 'react-declarative/components/Switch/Switch' {
     import ISwitchProps from 'react-declarative/components/Switch/model/ISwitchProps';
-    export const Switch: ({ items, history, NotFound, }: ISwitchProps) => import("react").ReactElement<{}, string | ((props: any) => import("react").ReactElement<any, string | any | (new (props: any) => import("react").Component<any, any, any>)> | null) | (new (props: any) => import("react").Component<any, any, any>)>;
+    export const Switch: ({ items, history, NotFound, }: ISwitchProps) => import("react").ReactElement<any, string | ((props: any) => import("react").ReactElement<any, string | any | (new (props: any) => import("react").Component<any, any, any>)> | null) | (new (props: any) => import("react").Component<any, any, any>)>;
     export default Switch;
 }
 
