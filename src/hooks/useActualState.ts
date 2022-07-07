af67a09d06058823cb35e29a3a33d3b1c6871a7e
Petr Tripolsky <tripolskypetr@gmail.com>
2022-07-07 20:08:24 +0300

    cleanup

--- src/hooks/useActualState.ts
+++ src/hooks/useActualState.ts
@@ -1,14 +1,10 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 
 export const useActualState = <S = undefined>(initialState?: S | (() => S)) => {
 
     const [state, setState] = useState<S>(initialState!);
 
     const stateRef = useRef(state);
-
-    useEffect(() => {
-        stateRef.current = state;
-    }, [state])
 
     const handleState: typeof setState = (dispatch) => {
         setState((prevState) => {
