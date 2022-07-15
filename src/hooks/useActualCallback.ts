9fea6058764bdfa92eb74e9db3e9f57efffb9e87
Petr Tripolsky <tripolskypetr@gmail.com>
2022-07-16 01:18:11 +0300

    actual-callback-ref

--- src/hooks/useActualCallback.ts
+++ src/hooks/useActualCallback.ts
@@ -1,11 +1,11 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useCallback } from "react";
 
 export const useActualCallback = <T extends (...args: any[]) => any>(run: T): T => {
     const executeRef = useRef<T>(run);
     useEffect(() => {
         executeRef.current = run;
     }, [run]);
-    return ((...args: any[]) => executeRef.current(...args)) as T;
+    return useCallback(((...args: any[]) => executeRef.current(...args)) as T, []);
 };
 
 export default useActualCallback;
