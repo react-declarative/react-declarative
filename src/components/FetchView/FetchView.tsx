2900a48f769d149d290dc8983b8aaddf42db4aab
Petr Tripolsky <tripolskypetr@gmail.com>
2022-11-21 16:10:42 +0300

    fetchview-typedef-patch

--- src/components/FetchView/FetchView.tsx
+++ src/components/FetchView/FetchView.tsx
@@ -13,18 +13,40 @@ import classNames from '../../utils/classNames';
 
 type Result = React.ReactNode | void;
 
-export interface IFetchViewProps<P extends any = object, A = any, B = any, C = any, D = any, E = any, F = any, G = any, H = any, I = any, J = any> extends Omit<IAsyncProps<P>, keyof {
+type ObjectState<P extends any = object, A = any> = ((payload: P) => Promise<A>)
+    | ((payload: P) => A);
+
+type TupleState<P extends any = object, A = any, B = any, C = any, D = any, E = any, F = any, G = any, H = any, I = any, J = any>  = 
+    ((payload: P) => readonly [Promise<A>?, Promise<B>?, Promise<C>?, Promise<D>?, Promise<E>?, Promise<F>?, Promise<G>?, Promise<H>?, Promise<I>?, Promise<J>?])
+        | ((payload: P) => readonly [A?, B?, C?, D?, E?, F?, G?, H?, I?, J?])
+        | ((payload: P) => [Promise<A>?, Promise<B>?, Promise<C>?, Promise<D>?, Promise<E>?, Promise<F>?, Promise<G>?, Promise<H>?, Promise<I>?, Promise<J>?])
+        | ((payload: P) => [A?, B?, C?, D?, E?, F?, G?, H?, I?, J?]);
+
+type UnknownState<P extends any = object, A = any, B = any, C = any, D = any, E = any, F = any, G = any, H = any, I = any, J = any>  = 
+    ObjectState<P, A>
+        | TupleState<P, A, B, C, D, E, F, G, H, I, J>;
+
+type Fn = (...args: any) => any;
+
+export interface IFetchViewProps<P extends any = object, 
+    A = any,
+    B = any,
+    C = any,
+    D = any,
+    E = any,
+    F = any,
+    G = any,
+    H = any,
+    I = any,
+    J = any,
+    FN extends Fn = UnknownState<P, A, B, C, D, E, F, G, H, I, J>
+> extends Omit<IAsyncProps<P>, keyof {
     children: never;
 }> {
     animation?: IRevealProps['animation'];
     className?: string;
     style?: React.CSSProperties;
-    state: ((payload: P) => readonly [Promise<A>?, Promise<B>?, Promise<C>?, Promise<D>?, Promise<E>?, Promise<F>?, Promise<G>?, Promise<H>?, Promise<I>?, Promise<J>?])
-        | ((payload: P) => readonly [A?, B?, C?, D?, E?, F?, G?, H?, I?, J?])
-        | ((payload: P) => [Promise<A>?, Promise<B>?, Promise<C>?, Promise<D>?, Promise<E>?, Promise<F>?, Promise<G>?, Promise<H>?, Promise<I>?, Promise<J>?])
-        | ((payload: P) => [A?, B?, C?, D?, E?, F?, G?, H?, I?, J?])
-        | ((payload: P) => Promise<A>)
-        | ((payload: P) => A);
+    state: ReturnType<FN> extends A | Promise<A> ? TupleState<P, A, B, C, D, E, F, J, H, I, J> : ObjectState<P, A>;
     children: (a: A, b: B, c: C, d: D, e: E, f: F, g: G, h: H, i: I, j: J) => Promise<Result> | Result;
 };
 
