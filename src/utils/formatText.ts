ae6a493d171e79314ee8ad677e6c0d084bd03543
Petr Tripolsky <tripolskypetr@gmail.com>
2022-12-25 21:14:42 +0300

    text-format-patch

--- src/utils/formatText.ts
+++ src/utils/formatText.ts
@@ -44,14 +44,13 @@ export const formatText = (raw: string, template: string, {
     let idx = 0;
     let result = '';
     for (const char of template) {
-        if (char === symbol || char === raw[idx]) {
+        if (idx === raw.length) {
+            break;
+        } else if (char === symbol || char === raw[idx]) {
             result += raw[idx];
             idx++;
         } else {
             result += char;
-        }
-        if (idx === raw.length) {
-            break;
         }
     }
     return result;
