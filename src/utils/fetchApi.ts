5acd3c17fec0928ed1d827c43543cf6d0d41e3bc
Petr Tripolsky <tripolskypetr@gmail.com>
2023-04-23 23:21:23 +0500

    icons-patch

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -9,11 +9,21 @@ export class FetchError extends Error {
     }
 };
 
+const PAYLOAD_METHODS: any[] = ['POST', 'PUT', 'PATCH'];
+
 export const fetchApi = async <T = any>(input: RequestInfo | URL, init?: RequestInit): Promise<T> => {
     const request = input instanceof URL ? input.toString() : input;
     let response: Response | undefined = undefined;
     try {
-        response = await fetch(request, init);
+        response = await fetch(request, {
+            ...init,
+            headers: {
+                ...(PAYLOAD_METHODS.includes(init?.method?.toUpperCase()) && {
+                    "Content-Type": "application/json",
+                }),
+                ...init?.headers,
+            },
+        });
         return await response.json();
     } catch (error: any) {
         throw new FetchError(
