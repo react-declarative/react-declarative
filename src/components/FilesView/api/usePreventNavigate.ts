562c4e1469657bb4581b0b86b02647f0ee24df5d
Petr Tripolsky <tripolskypetr@gmail.com>
2023-06-17 01:27:14 +0500

    better-prevent-navigate

--- src/components/FilesView/api/usePreventNavigate.ts
+++ src/components/FilesView/api/usePreventNavigate.ts
@@ -1,28 +1,42 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { MemoryHistory, BrowserHistory, HashHistory } from 'history';
+
+import useActualCallback from '../../../hooks/useActualCallback';
+import useActualValue from '../../../hooks/useActualValue';
 
 const LEAVE_MESSAGE = 'The form contains unsaved changes. Continue?';
 
 interface IParams {
     history: MemoryHistory | BrowserHistory | HashHistory;
-    check: () => boolean;
+    onLoadStart?: () => void;
+    onLoadEnd?: (isOk: boolean) => void;
+    check?: () => boolean;
 }
 
 export const usePreventNavigate = ({
     history,
-    check,
+    check = () => false,
+    onLoadStart,
+    onLoadEnd,
 }: IParams) => {
+
+    const [loading, setLoading] = useState(0);
+
+    const check$ = useActualCallback(check);
+    const loading$ = useActualValue(loading);
+
     useEffect(() => history.block(
         ({ retry }) => {
-            if (!check()) {
+            if (!check$() && !loading$.current) {
                 retry();
             }
         }
     ), []);
+
     useEffect(() => {
         const handler = (e: any) => {
-            if (check()) {
+            if (check$() || loading$.current) {
                 e.preventDefault();
                 return LEAVE_MESSAGE;
             }
@@ -33,6 +47,17 @@ export const usePreventNavigate = ({
             window.onbeforeunload = null;
         };
     }, []);
+
+    return {
+        handleLoadStart: () => {
+            onLoadStart && onLoadStart();
+            setLoading((loading) => loading + 1);
+        },
+        handleLoadEnd: (isOk: boolean) => {
+            onLoadEnd && onLoadEnd(isOk);
+            setLoading((loading) => loading - 1);
+        },
+    };
 };
 
 export default usePreventNavigate;
