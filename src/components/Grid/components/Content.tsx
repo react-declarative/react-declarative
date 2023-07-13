426d79065ccd2451a2955c09b63917d36f6ef32a
Petr Tripolsky <tripolskypetr@gmail.com>
2023-07-13 15:39:53 +0500

    rd-inc

--- src/components/Grid/components/Content.tsx
+++ src/components/Grid/components/Content.tsx
@@ -45,7 +45,7 @@ interface IContentProps {
 
 const useStyles = makeStyles()({
   content: {
-    height: "calc(100% - (35px * 2))"
+    height: "calc(100% - 35px)"
   },
   noData: {
     display: "flex",
