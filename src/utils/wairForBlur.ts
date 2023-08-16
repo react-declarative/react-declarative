e37580fce6fec8cddc30ccfd7467ee6ab757586b
Petr Tripolsky <tripolskypetr@gmail.com>
2023-08-16 17:43:42 +0500

    cleanup

--- src/utils/wairForBlur.ts
+++ src/utils/wairForBlur.ts
@@ -1,14 +1,31 @@
-export const waitForBlur = (ref: HTMLElement) => new Promise<void>((res) => {
+export const waitForBlur = (ref: HTMLElement) =>
+  new Promise<void>((res) => {
     const interval = setInterval(() => {
-        /**
-         * Для поддержки группы полей, также проверяем наличие родителя сквозь
-         * вложенность через HTMLElement.prototype.contains()
-         */
-        if (document.activeElement !== ref && !ref.contains(document.activeElement)) {
-            clearInterval(interval);
-            res();
-        }
+      /**
+       * Для поддержки группы полей, также проверяем наличие родителя сквозь
+       * вложенность через HTMLElement.prototype.contains()
+       */
+      if (
+        document.activeElement !== ref &&
+        !ref.contains(document.activeElement)
+      ) {
+        clearInterval(interval);
+        res();
+      }
     }, 50);
-});
+    const elements = ref.querySelectorAll<HTMLInputElement>("input");
+    const dispose = () => {
+      clearInterval(interval);
+      elements.forEach((element) =>
+        element.removeEventListener("blur", dispose)
+      );
+      res();
+    };
+    elements.forEach((input) => {
+      input.addEventListener("blur", dispose, {
+        once: true,
+      });
+    });
+  });
 
 export default waitForBlur;
