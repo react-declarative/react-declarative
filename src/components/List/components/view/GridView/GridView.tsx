a0a1c55c4a298e5ee66d7666a89787e0b49a2053
Petr Tripolsky <tripolskypetr@gmail.com>
2023-09-19 13:29:35 +0500

    list-patch

--- src/components/List/components/view/GridView/GridView.tsx
+++ src/components/List/components/view/GridView/GridView.tsx
@@ -1,24 +1,27 @@
 import * as React from "react";
-import { useEffect, Fragment } from 'react';
+import { useEffect, Fragment } from "react";
 
-import { makeStyles } from '../../../../../styles';
+import { makeStyles } from "../../../../../styles";
 
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableHead from '@mui/material/TableHead';
-import TableContainer from '@mui/material/TableContainer';
-import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material/TableCell';
-import CircularProgress from '@mui/material/CircularProgress';
-import Typography from '@mui/material/Typography';
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableHead from "@mui/material/TableHead";
+import TableContainer from "@mui/material/TableContainer";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import CircularProgress from "@mui/material/CircularProgress";
+import Typography from "@mui/material/Typography";
 
-import TablePagination from '../../TablePagination';
+import TablePagination from "../../TablePagination";
 
-import IListProps, { IListState, IListCallbacks } from '../../../../../model/IListProps';
-import IAnything from '../../../../../model/IAnything';
-import IRowData from '../../../../../model/IRowData';
+import IListProps, {
+  IListState,
+  IListCallbacks,
+} from "../../../../../model/IListProps";
+import IAnything from "../../../../../model/IAnything";
+import IRowData from "../../../../../model/IRowData";
 
 import ConstraintView from "../../../../ConstraintView";
 
@@ -38,46 +41,52 @@ const ROWS_PER_PAGE = [10, 25, 50];
 
 const useStyles = makeStyles()((theme, _, classes) => ({
   root: {
-    position: 'relative',
+    position: "relative",
     background: theme.palette.background.paper,
-    [`& .${classes['noBorder']}`]: {
-      paddingLeft: '0 !important',
-      paddingRight: '0 !important',
+    [`& .${classes["noBorder"]}`]: {
+      paddingLeft: "0 !important",
+      paddingRight: "0 !important",
     },
   },
   noBorder: {
-    border: 'none !important',
+    border: "none !important",
   },
   tableHead: {
-    position: 'sticky',
+    position: "sticky",
     top: -1,
     zIndex: 1,
     background: theme.palette.background.paper,
   },
 }));
 
-interface IGridViewProps<FilterData extends {} = IAnything, RowData extends IRowData = IAnything> extends
-  Omit<IListProps<FilterData, RowData>, keyof {
-    ref: never;
-    limit: never;
-    chips: never;
-    search: never;
-    filterData: never;
-    isChooser: never;
-    payload: never;
-  }>,
-  IListState<FilterData, RowData>,
-  IListCallbacks<FilterData, RowData> {
+interface IGridViewProps<
+  FilterData extends {} = IAnything,
+  RowData extends IRowData = IAnything
+> extends Omit<
+      IListProps<FilterData, RowData>,
+      keyof {
+        ref: never;
+        limit: never;
+        chips: never;
+        search: never;
+        filterData: never;
+        isChooser: never;
+        payload: never;
+      }
+    >,
+    IListState<FilterData, RowData>,
+    IListCallbacks<FilterData, RowData> {
   className?: string;
   style?: React.CSSProperties;
-  listChips: IListProps['chips'];
+  listChips: IListProps["chips"];
 }
 
 export const GridView = <
   FilterData extends {} = IAnything,
-  RowData extends IRowData = IAnything,
-  >(props: IGridViewProps<FilterData, RowData>) => {
-
+  RowData extends IRowData = IAnything
+>(
+  props: IGridViewProps<FilterData, RowData>
+) => {
   const { classes } = useStyles();
 
   const scrollManager = useScrollManager();
@@ -94,20 +103,34 @@ export const GridView = <
     withInitialLoader = false,
   } = props;
 
-  const {
-    handleLimitChange,
-    handlePageChange,
-  } = props;
+  const { handleLimitChange, handlePageChange } = props;
 
   const handleResize = () => constraintManager.clear();
 
   const handleDirtyLimitChange = (e: any) => handleLimitChange(e.target.value);
 
-  const handleDirtyPageChange = (_: any, newPage: number) => handlePageChange(newPage);
+  const handleDirtyPageChange = (_: any, newPage: number) =>
+    handlePageChange(newPage);
 
-  const renderPlaceholder = () => (
-    <TableCell className={classes.noBorder} colSpan={(columns.length * 2)  || 999} align="center">
-      <Stack direction="row" alignItems="center" justifyContent="center" gap={1}>
+  const renderPlaceholder = (width: number) => (
+    <TableCell
+      className={classes.noBorder}
+      sx={{ position: 'relative' }}
+      colSpan={columns.length * 2 || 999}
+      align="center"
+    >
+      <Stack
+        sx={{
+          position: 'sticky',
+          top: 0,
+          left: 0,
+          width,
+        }}
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        gap={1}
+      >
         {loading && <CircularProgress size={24} />}
         <Typography variant="body1">
           {loading ? "Loading" : "Nothing found"}
@@ -116,48 +139,49 @@ export const GridView = <
     </TableCell>
   );
 
-  useEffect(() => () => {
-    scrollManager.clear();
-  }, []);
+  useEffect(
+    () => () => {
+      scrollManager.clear();
+    },
+    []
+  );
 
   return (
-    <Container<FilterData, RowData>
-      {...props}
-      onResize={handleResize}
-    >
+    <Container<FilterData, RowData> {...props} onResize={handleResize}>
       {(params) => {
-
-        const { height, width, payload: { rows } } = params;
+        const {
+          height,
+          width,
+          payload: { rows },
+        } = params;
 
         const renderInner = (mode: DisplayMode) => (
           <>
             <TableHead className={classes.tableHead}>
-              <HeadRow
-                fullWidth={width}
-                mode={mode}
-              />
+              <HeadRow fullWidth={width} mode={mode} />
             </TableHead>
             <TableBody>
-              {(withLoader && loading) || (withInitialLoader && loading && rows.length === 0) || (!loading && rows.length === 0) ? (
-                <TableRow>
-                  {renderPlaceholder()}
-                </TableRow>
-              ) : rows.map((row, index) => (
-                <Fragment key={index}>
-                  <BodyRow
-                    fullWidth={width}
-                    row={row}
-                    mode={mode}
-                  />
-                </Fragment>
-              ))}
+              {(withLoader && loading) ||
+              (withInitialLoader && loading && rows.length === 0) ||
+              (!loading && rows.length === 0) ? (
+                <TableRow>{renderPlaceholder(width)}</TableRow>
+              ) : (
+                rows.map((row, index) => (
+                  <Fragment key={index}>
+                    <BodyRow fullWidth={width} row={row} mode={mode} />
+                  </Fragment>
+                ))
+              )}
             </TableBody>
           </>
         );
 
         return (
           <Box className={classes.root}>
-            <TableContainer ref={scrollManager.provideRef} style={{ height: height - PAGINATION_HEIGHT, width }}>
+            <TableContainer
+              ref={scrollManager.provideRef}
+              style={{ height: height - PAGINATION_HEIGHT, width }}
+            >
               <Table stickyHeader>
                 <ConstraintView
                   phoneView={() => renderInner(DisplayMode.Phone)}
