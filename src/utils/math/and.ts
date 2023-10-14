7750e3e5a4d66c83d971b1af129b1349bbbbb909
Petr Tripolsky <tripolskypetr@gmail.com>
2023-10-14 13:29:26 +0500

    patch

--- src/utils/math/and.ts
+++ src/utils/math/and.ts
@@ -2,10 +2,14 @@ type Value = number | boolean;
 
 export const and = <T = Promise<Value>>(...args: T[]): T => {
     if (args.some((arg) => arg instanceof Promise)) {
-        return new Promise<boolean>(async (res) => {
-            const items = await Promise.all(args);
-            const result = items.reduce<boolean>((acm, cur) => Boolean(acm && cur), true);
-            res(result);
+        return new Promise<boolean>(async (res, rej) => {
+            try {
+                const items = await Promise.all(args);
+                const result = items.reduce<boolean>((acm, cur) => Boolean(acm && cur), true);
+                res(result);
+            } catch {
+                rej(false);
+            }
         }) as unknown as T;
     }
     return args.reduce<boolean>((acm, cur) => Boolean(acm && cur), true) as unknown as T;
--- src/utils/math/or.ts
+++ src/utils/math/or.ts
@@ -2,10 +2,14 @@ type Value = number | boolean;
 
 export const or = <T = Promise<Value>>(...args: T[]): T => {
     if (args.some((arg) => arg instanceof Promise)) {
-        return new Promise<boolean>(async (res) => {
-            const items = await Promise.all(args);
-            const result = items.reduce<boolean>((acm, cur) => Boolean(acm || cur), false);
-            res(result);
+        return new Promise<boolean>(async (res, rej) => {
+            try {
+                const items = await Promise.all(args);
+                const result = items.reduce<boolean>((acm, cur) => Boolean(acm || cur), false);
+                res(result);
+            } catch {
+                rej(false);
+            }
         }) as unknown as T;
     }
     return args.reduce<boolean>((acm, cur) => Boolean(acm || cur), false) as unknown as T;
