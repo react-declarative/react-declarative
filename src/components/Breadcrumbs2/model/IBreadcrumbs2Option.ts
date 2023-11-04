ba8440a2749f1f2e781d6f866f0253c2529a70fb
Petr Tripolsky <tripolskypetr@gmail.com>
2023-11-04 22:49:27 +0500

    label-icon

--- src/components/Breadcrumbs2/model/IBreadcrumbs2Option.ts
+++ src/components/Breadcrumbs2/model/IBreadcrumbs2Option.ts
@@ -6,8 +6,10 @@ import Breadcrumbs2Type from "./Breadcrumbs2Type";
 export interface IBreadcrumbs2Option<Data = IAnything> extends Omit<IOption, keyof {
     isVisible: never;
     isDisabled: never;
+    label: never;
 }> {
     type: Breadcrumbs2Type;
+    label?: React.ReactNode;
     compute?: (payload: Data) => (Promise<string> | string);
     isVisible?: (payload: Data) => (Promise<boolean> | boolean);
     isDisabled?: (payload: Data) => (Promise<boolean> | boolean);
