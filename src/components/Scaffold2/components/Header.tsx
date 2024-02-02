30759357f4631ee2e410e3a754638a537a3d44f5
Petr Tripolsky <tripolskypetr@gmail.com>
2024-02-02 23:56:39 +0500

    patch

--- src/components/Scaffold2/components/Header.tsx
+++ src/components/Scaffold2/components/Header.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useMemo, useCallback } from "react";
 import { SxProps } from "@mui/material";
 
-import { makeStyles } from '../../../styles';
+import { makeStyles } from "../../../styles";
 
 import IconButton from "@mui/material/IconButton";
 import AppBar from "@mui/material/AppBar";
@@ -29,7 +29,8 @@ import Payload from "../model/Payload";
 import deepFlat from "../utils/deepFlat";
 import idToLabel from "../utils/idToLabel";
 
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu";
+import useMediaContext from "../../../hooks/useMediaContext";
 
 interface IHeaderProps<T = Payload> extends StackProps {
   className?: string;
@@ -55,11 +56,11 @@ const TAB_HEIGHT = 48;
 const useStyles = makeStyles()({
   tabsRoot: {
     minHeight: TAB_HEIGHT,
-    height: TAB_HEIGHT
+    height: TAB_HEIGHT,
   },
   tabRoot: {
     minHeight: TAB_HEIGHT,
-    height: TAB_HEIGHT
+    height: TAB_HEIGHT,
   },
 });
 
@@ -82,8 +83,14 @@ export const Header = <T extends Payload = Payload>({
   actions = [],
   ...otherProps
 }: IHeaderProps<T>) => {
+  const {
+    dense = false,
+    fixedHeader = false,
+    BeforeActionMenu,
+    AfterAppName,
+  } = usePropsContext();
 
-  const { dense = false, fixedHeader = false, BeforeActionMenu, AfterAppName } = usePropsContext();
+  const { isPhone } = useMediaContext();
 
   const { classes } = useStyles();
 
@@ -98,7 +105,7 @@ export const Header = <T extends Payload = Payload>({
     return {
       id,
       path,
-      label: id === 'unknown' ? appName : label,
+      label: id === "unknown" ? appName : label,
       tabs: tabs as IScaffold2TabInternal[],
     };
   }, [activeOptionPath, options, appName]);
@@ -115,9 +122,12 @@ export const Header = <T extends Payload = Payload>({
     return !!tabs.filter(({ visible }) => visible).length;
   }, [tabs]);
 
-  const handleTabChange = useCallback((path: string, tabId: string) => {
-    onTabChange && onTabChange(path, tabId, id);
-  }, [path, id]);
+  const handleTabChange = useCallback(
+    (path: string, tabId: string) => {
+      onTabChange && onTabChange(path, tabId, id);
+    },
+    [path, id]
+  );
 
   return (
     <>
@@ -125,16 +135,16 @@ export const Header = <T extends Payload = Payload>({
         className={className}
         style={style}
         sx={{
-          width: '100%',
+          width: "100%",
           zIndex: 999,
           ...(fixedHeader && {
-            position: 'fixed',
+            position: "fixed",
             top: 0,
-            left: isMobile ? 0 : '256px',
-            width: isMobile ? '100%' : 'calc(100% - 256px)',
+            left: isMobile ? 0 : "256px",
+            width: isMobile ? "100%" : "calc(100% - 256px)",
           }),
           ...(!fixedHeader && {
-            position: 'sticky',
+            position: "sticky",
             top: 0,
             left: 0,
             right: 0,
@@ -152,15 +162,20 @@ export const Header = <T extends Payload = Payload>({
           elevation={0}
           sx={{
             zIndex: 0,
-            pb: (hasTabs && !isMobile && !dense) ? 2 : 0,
+            pb: hasTabs && !isMobile && !dense ? 2 : 0,
           }}
         >
           <Toolbar variant={dense ? "dense" : "regular"}>
             <Grid container alignItems="center" wrap="nowrap" spacing={1}>
-              <Grid sx={{ display: !dense ? { sm: 'none', xs: 'block' } : undefined }} item>
+              <Grid
+                sx={{
+                  display: !dense ? { sm: "none", xs: "block" } : undefined,
+                }}
+                item
+              >
                 <IconButton
                   sx={{
-                    pointerEvents: isMobile ? 'inherit' : 'none',
+                    pointerEvents: isMobile ? "inherit" : "none",
                   }}
                   color="inherit"
                   aria-label="open drawer"
@@ -174,7 +189,13 @@ export const Header = <T extends Payload = Payload>({
                 {dense ? (
                   <>
                     {!hasTabs && (
-                      <Typography color="inherit" variant="h5" component="h1">
+                      <Typography
+                        display={isPhone ? "none" : undefined}
+                        whiteSpace="nowrap"
+                        color="inherit"
+                        variant="h5"
+                        component="h1"
+                      >
                         {label || appName}
                       </Typography>
                     )}
@@ -190,47 +211,53 @@ export const Header = <T extends Payload = Payload>({
                       >
                         {tabs
                           .filter(({ visible }) => visible)
-                          .map(({ id, label, icon: Icon, disabled, path }, idx) => (
-                            <Tab
-                              sx={{
-                                minWidth: 128,
-                                fontWeight: 'bold',
-                              }}
-                              key={`${id}-${idx}`}
-                              value={path}
-                              label={label || idToLabel(id)}
-                              onClick={() => handleTabChange(path, id)}
-                              disabled={disabled}
-                              icon={Icon && <Icon />}
-                              iconPosition="start"
-                              classes={{
-                                root: classes.tabRoot
-                              }}
-                            />
-                          )
+                          .map(
+                            (
+                              { id, label, icon: Icon, disabled, path },
+                              idx
+                            ) => (
+                              <Tab
+                                sx={{
+                                  minWidth: 128,
+                                  fontWeight: "bold",
+                                }}
+                                key={`${id}-${idx}`}
+                                value={path}
+                                label={label || idToLabel(id)}
+                                onClick={() => handleTabChange(path, id)}
+                                disabled={disabled}
+                                icon={Icon && <Icon />}
+                                iconPosition="start"
+                                classes={{
+                                  root: classes.tabRoot,
+                                }}
+                              />
+                            )
                           )}
                       </Tabs>
                     )}
                   </>
                 ) : (
-                  <Typography color="inherit" variant="h5" component="h1">
+                  <Typography
+                    display={isPhone ? "none" : undefined}
+                    whiteSpace="nowrap"
+                    color="inherit"
+                    variant="h5"
+                    component="h1"
+                  >
                     {label || appName}
                   </Typography>
                 )}
               </Grid>
               {!!AfterAppName && (
                 <Grid item>
-                  <AfterAppName
-                    payload={payload}
-                  />
+                  <AfterAppName payload={payload} />
                 </Grid>
               )}
               <Grid item xs />
               {!!BeforeActionMenu && (
                 <Grid item>
-                  <BeforeActionMenu
-                    payload={payload}
-                  />
+                  <BeforeActionMenu payload={payload} />
                 </Grid>
               )}
               {!!actions?.length && (
@@ -238,7 +265,7 @@ export const Header = <T extends Payload = Payload>({
                   <ActionMenu
                     transparent
                     sx={{
-                      color: 'unset !important',
+                      color: "unset !important",
                       mr: -2,
                     }}
                     BeforeContent={BeforeMenuContent}
@@ -283,7 +310,7 @@ export const Header = <T extends Payload = Payload>({
                 .map(({ id, label, icon: Icon, disabled, path }, idx) => (
                   <Tab
                     sx={{
-                      minWidth: '128px',
+                      minWidth: "128px",
                     }}
                     key={`${id}-${idx}`}
                     value={path}
@@ -293,23 +320,28 @@ export const Header = <T extends Payload = Payload>({
                     icon={Icon && <Icon />}
                     iconPosition="start"
                     classes={{
-                      root: classes.tabRoot
+                      root: classes.tabRoot,
                     }}
                   />
-                )
-                )}
+                ))}
             </Tabs>
           </AppBar>
         )}
         {!!loading && (
           <Box
             sx={{
-              marginTop: '-4px',
+              marginTop: "-4px",
             }}
           >
             <LinearProgress
-              variant={loading as unknown as number > 1 ? "determinate" : "indeterminate"}
-              value={loading as unknown as number > 1 ? Number(loading) : undefined}
+              variant={
+                (loading as unknown as number) > 1
+                  ? "determinate"
+                  : "indeterminate"
+              }
+              value={
+                (loading as unknown as number) > 1 ? Number(loading) : undefined
+              }
               color="primary"
             />
           </Box>
@@ -319,23 +351,29 @@ export const Header = <T extends Payload = Payload>({
         <Box
           sx={{
             ...(dense && {
-              paddingBottom: '48px',
+              paddingBottom: "48px",
             }),
             ...(!dense && {
-              ...isMobile ? {
-                ...hasTabs ? {
-                  paddingBottom: '106px',
-                } : {
-                  paddingBottom: '56px',
-                }
-              } : {
-                ...hasTabs ? {
-                  paddingBottom: '128px',
-                } : {
-                  paddingBottom: '64px',
-                }
-              }
-            })
+              ...(isMobile
+                ? {
+                    ...(hasTabs
+                      ? {
+                          paddingBottom: "106px",
+                        }
+                      : {
+                          paddingBottom: "56px",
+                        }),
+                  }
+                : {
+                    ...(hasTabs
+                      ? {
+                          paddingBottom: "128px",
+                        }
+                      : {
+                          paddingBottom: "64px",
+                        }),
+                  }),
+            }),
           }}
         />
       )}
