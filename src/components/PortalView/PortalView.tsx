bb631c7bac640bc30f848835d36630bc32e1788f
Petr Tripolsky <tripolskypetr@gmail.com>
2024-03-23 13:29:10 +0500

    docs

--- src/components/PortalView/PortalView.tsx
+++ src/components/PortalView/PortalView.tsx
@@ -1,14 +1,33 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+/**
+ * Represents the properties for the PortalView component.
+ */
 interface IPortalViewProps {
   children: React.ReactNode;
 }
 
+/**
+ * Represents a portal view component.
+ * @class
+ */
 export class PortalView extends React.Component<IPortalViewProps> {
 
   element: HTMLDivElement | null = null;
 
+  /**
+   * Method componentWillUnmount
+   *
+   * This method is called when the component is about to be unmounted (removed) from the DOM.
+   * It performs clean-up tasks, if any, to avoid memory leaks or unwanted effects after the component is removed.
+   *
+   * This method checks if 'this.element' (assumed to be a DOM element) exists, and if so,
+   * removes it from the document body using 'document.body.removeChild()'.
+   * Finally, it sets 'this.element' to null to release any references to the removed element.
+   *
+   * @return
+   */
   componentWillUnmount() {
     if (this.element) {
       document.body.removeChild(this.element);
@@ -16,6 +35,11 @@ export class PortalView extends React.Component<IPortalViewProps> {
     this.element = null;
   };
 
+  /**
+   * Renders the provided children into a portal, within a dynamically created div element appended to the document body.
+   *
+   * @return - The resulting React element after rendering the children into the portal.
+   */
   render() {
     if (!this.element) {
       this.element = document.createElement('div');
