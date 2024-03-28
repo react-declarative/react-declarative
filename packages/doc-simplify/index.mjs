5f0f1e55043ff025a1164e6ab5c83a4017193e89
Petr Tripolsky <tripolskypetr@gmail.com>
2024-03-29 00:55:14 +0500

    gpt4all-begin

--- packages/doc-simplify/index.mjs
+++ packages/doc-simplify/index.mjs
@@ -1,46 +1,65 @@
-import OpenAI from 'openai';
-import dotenv from 'dotenv';
-import fs from 'fs';
+import { createCompletion, loadModel } from "gpt4all";
+
+import { exec } from 'child_process';
+import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
 
-const GPT_PROMPT = 'Please write a summary for that React component with several sentences:';
-
-const openai = new OpenAI({
-  apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
-});
+const GPT_PROMPT =
+  "Please write a summary for that React component with several sentences in more human way";
 
 async function main() {
+  const model = await loadModel("orca-mini-3b-gguf2-q4_0.gguf", {
+    verbose: true,
+    device: "gpu",
+    nCtx: 2048,
+  });
+  const data = fs.readFileSync("../../docs/code/Components.md").toString();
 
-  const data = fs.readFileSync('../../docs/code/Components.md').toString();
-
-  const chunks = data.split('# ').filter(v => v).map(v => " # "+v);
+  const chunks = data
+    .split("# ")
+    .filter((v) => v)
+    .map((v) => " # " + v);
 
   const result = [];
   let error = null;
 
+  console.log('Creating chat session');
+
+  const chat = await model.createChatSession({
+    temperature: 0.8,
+    systemPrompt: `### System:\n${GPT_PROMPT}.\n\n`,
+  });
+
+  console.time('Documentation');
+
   try {
     for (const chunk of chunks) {
-        const header = chunk.split('\n')[0].trim();
-        console.log(`Generating header=${header}`)
-        const content = [GPT_PROMPT, chunk].join('\n');
-        const chatCompletion = await openai.chat.completions.create({
-            messages: [{ role: 'user', content }],
-            model: 'gpt-3.5-turbo',
-        });
-        const result = chatCompletion.choices[0].message;
-        result.push([header, result].join('\n'));
-      }
+      const header = chunk.split("\n")[0].split('#').join('').trim();
+      console.log(`Generating header=${header}`);
+
+      const res1 = await createCompletion(chat, chunk);
+
+      const output = res1.choices[0].message.content;
+
+      result.push(output.trim());
+      fs.writeFileSync("./Result.md", result.join("\n\n").toString());
+    }
   } catch (e) {
     error = e;
   } finally {
-    fs.writeFileSync('./Result.md', result.join('\n\n').toString());
-    console.log('Created Result.md');
+    fs.writeFileSync("./Result.md", result.join("\n\n").toString());
+    console.log("Created Result.md");
     if (error) {
-        console.log(`Documentation creation failed with exception error=${error.toString()}`);
+      console.log(
+        `Documentation creation failed with exception error=${error.toString()}`
+      );
     }
   }
 
+  console.timeEnd('Documentation');
+
 }
 
-main();
+main().then(() => exec("shutdown \s \t 1"));
