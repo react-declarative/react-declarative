c0112e09f6a6f8f61469b817612b1a5784e5b7ab
Petr Tripolsky <tripolskypetr@gmail.com>
2024-05-22 23:05:00 +0500

    patch

--- src/api/iterateUnion.ts
+++ src/api/iterateUnion.ts
@@ -1,37 +1,27 @@
 import IRowData, { RowId } from "../model/IRowData";
 
-async function* makeIterator<T extends IRowData = IRowData>(
-    iterators: AsyncGenerator<T | T[], void, unknown>[],
-    limit: number,
-    offset: number,
-) {
-    for (const iterator of iterators) {
-        for await (const chunk of iterator) {
-            const rows = [chunk].flatMap(v => v);
-            for (const row of rows) {
-                if (offset > 0) {
-                    offset -= 1;
-                    continue;
-                }
-                if (limit > 0) {
-                    yield row;
-                    limit -= 1;
-                    continue;
-                }
-            }
-        }
-    }
-}
-
 export const iterateUnion = <T extends IRowData = IRowData>(iterators: AsyncGenerator<T | T[], void, unknown>[]) =>
     async function* (limit: number, offset: number) {
         const duplicateSet = new Set<RowId>();
-        for await (const row of makeIterator(iterators, limit, offset)) {
-            if (duplicateSet.has(row.id)) {
-                continue;
+        for (const iterator of iterators) {
+            for await (const chunk of iterator) {
+                const rows = [chunk].flatMap(v => v);
+                for (const row of rows) {
+                    if (duplicateSet.has(row.id)) {
+                        continue;
+                    }
+                    if (offset > 0) {
+                        offset -= 1;
+                        continue;
+                    }
+                    if (limit > 0) {
+                        duplicateSet.add(row.id);
+                        yield row;
+                        limit -= 1;
+                        continue;
+                    }
+                }
             }
-            duplicateSet.add(row.id);
-            yield row;
         }
     };
 
