4e88c4d040cd011e196e250ad186f1141a06b6e8
Petr Tripolsky <tripolskypetr@gmail.com>
2024-05-24 20:15:47 +0500

    cleanup

--- src/utils/hof/obsolete.ts
+++ src/utils/hof/obsolete.ts
@@ -1,13 +1,12 @@
+import randomString from "../randomString";
 
-const RANDOM_CODE = (Math.random() + 1).toString(36).substring(7);
+const RANDOM_CODE = randomString();
 const OBSOLETE_PROMPT = `Method marked as obsolete. Type ${RANDOM_CODE} to continue`;
 
 export const obsolete = <T extends any = any, P extends any[] = any[]>(run: (...args: P) => Promise<T>) => {
-    let isPass = false;
     return async (...args: P): Promise<T> => {
         if ('prompt' in window) {
-            if (isPass || window.prompt(OBSOLETE_PROMPT) === RANDOM_CODE) {
-                isPass = true;
+            if (window.prompt(OBSOLETE_PROMPT) === RANDOM_CODE) {
                 return await run(...args);
             }
         }
