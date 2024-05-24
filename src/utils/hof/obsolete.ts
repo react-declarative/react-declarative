3c5cb2ed54ca0bdb1acdfd4649c856e65dc1df74
Petr Tripolsky <tripolskypetr@gmail.com>
2024-05-24 18:17:24 +0500

    patch

--- src/utils/hof/obsolete.ts
+++ src/utils/hof/obsolete.ts
@@ -2,13 +2,17 @@
 const RANDOM_CODE = (Math.random() + 1).toString(36).substring(7);
 const OBSOLETE_PROMPT = `Method marked as obsolete. Type ${RANDOM_CODE} to continue`;
 
-export const obsolete = <T extends any = any, P extends any[] = any[]>(run: (...args: P) => Promise<T>) => async (...args: P): Promise<T> => {
-    if ('prompt' in window) {
-        if (window.prompt(OBSOLETE_PROMPT) === RANDOM_CODE) {
-            return await run(...args);
+export const obsolete = <T extends any = any, P extends any[] = any[]>(run: (...args: P) => Promise<T>) => {
+    let isPass = false;
+    return async (...args: P): Promise<T> => {
+        if ('prompt' in window) {
+            if (isPass || window.prompt(OBSOLETE_PROMPT) === RANDOM_CODE) {
+                isPass = true;
+                return await run(...args);
+            }
         }
-    }
-    throw new Error('react-declarative obsolete hof call');
+        throw new Error('react-declarative obsolete hof call');
+    };
 };
 
 export default obsolete;
