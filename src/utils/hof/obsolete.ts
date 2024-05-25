97e5e438e14e7aedb1d2e48b0ecb34ebd8d43e2d
Petr Tripolsky <tripolskypetr@gmail.com>
2024-05-25 15:57:14 +0500

    patch

--- src/utils/hof/obsolete.ts
+++ src/utils/hof/obsolete.ts
@@ -3,8 +3,28 @@ import randomString from "../randomString";
 const RANDOM_CODE = randomString();
 const OBSOLETE_PROMPT = `Method marked as obsolete. Type ${RANDOM_CODE} to continue`;
 
+type Function = (...args: any[]) => any;
+
+const getMethodName = (fn: Function, self: object | undefined): string => {
+    if (!self) {
+        return "unknown";
+    }
+    if (typeof self !== 'object') {
+        return "unknown";
+    }
+    for (const key of Object.keys(self)) {
+        if (self[key] === fn) {
+            return key;
+        }
+    }
+    return getMethodName(fn, Object.getPrototypeOf(self));
+};
+
 export const obsolete = <T extends any = any, P extends any[] = any[]>(run: (...args: P) => Promise<T>) => {
-    return async (...args: P): Promise<T> => {
+    let methodName = "";
+    const fn = async (...args: P): Promise<T> => {
+        methodName = methodName || getMethodName(fn, this);
+        console.log(`react-declarative obsolete hof call method_name=${methodName}`);
         if ('prompt' in window) {
             if (window.prompt(OBSOLETE_PROMPT) === RANDOM_CODE) {
                 return await run(...args);
@@ -12,6 +32,7 @@ export const obsolete = <T extends any = any, P extends any[] = any[]>(run: (...
         }
         throw new Error('react-declarative obsolete hof call');
     };
+    return fn;
 };
 
 export default obsolete;
