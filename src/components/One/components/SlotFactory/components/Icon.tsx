03a65a31c5685a626af7672e07def37cee030cd8
Petr Tripolsky <tripolskypetr@gmail.com>
2024-06-11 19:59:16 +0500

    patch

--- src/components/One/components/SlotFactory/components/Icon.tsx
+++ src/components/One/components/SlotFactory/components/Icon.tsx
@@ -1,10 +1,22 @@
 import * as React from 'react';
 
+import { makeStyles } from '../../../../../styles';
+
 import ActionIcon from '../../../../ActionIcon';
 
 import { IIconSlot } from '../../../slots/IconSlot';
 
 import ArrowForward from '@mui/icons-material/ArrowForward';
+
+const useStyles = makeStyles()({
+    root: {
+        flex: 1,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+    },
+});
 
 /**
  * Represents a icon component.
@@ -15,16 +27,20 @@ export const Icon = ({
     icon: Icon = ArrowForward,
     iconSize,
     iconColor,
-}: IIconSlot) => (
-    <ActionIcon
-        size={iconSize}
-        color={iconColor}
-        disabled={disabled}
-        onClick={click}
-    >
-        <Icon />
-    </ActionIcon>
-);
+}: IIconSlot) => {
+    const { classes } = useStyles();
+    return (
+        <ActionIcon
+            className={classes.root}
+            size={iconSize}
+            color={iconColor}
+            disabled={disabled}
+            onClick={click}
+        >
+            <Icon />
+        </ActionIcon>
+    );
+};
 
 export default Icon;
 
